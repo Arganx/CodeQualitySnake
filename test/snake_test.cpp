e4e9d184e90f9e9e257f24cfce991b73a1a43f62
Arganx <kartas345@gmail.com>
2023-11-05 00:50:55 +0100

    Var const added

--- test/snake_test.cpp
+++ test/snake_test.cpp
@@ -2,8 +2,13 @@
 #include "segment.hpp"
 #include "snake.hpp"
 #include "gtest/gtest.h"
+#include <sys/types.h>
 
 namespace {
+
+constexpr u_int8_t k0{0U};
+constexpr u_int8_t k1{1U};
+constexpr u_int8_t k3{3U};
 
 class SnakeFixture : public ::testing::Test {
 private:
@@ -15,7 +20,7 @@ protected:
 
 TEST(SnakeTest, DefaultSnakeCreation) {
   Game::Snake snake{};
-  EXPECT_EQ(snake.getSnakeSegments().size(), 0);
+  EXPECT_EQ(snake.getSnakeSegments().size(), k0);
   EXPECT_EQ(snake.getHeadPosition(), std::nullopt);
 }
 
@@ -23,17 +28,17 @@ TEST(SnakeTest, CanCreateSnakeWithSegment) {
   Game::BoardPosition position{};
   Game::Segment segment{position};
   Game::Snake snake{segment};
-  EXPECT_EQ(snake.getSnakeSegments().size(), 1);
+  EXPECT_EQ(snake.getSnakeSegments().size(), k1);
   EXPECT_EQ(snake.getHeadPosition()->get(), position);
 }
 
 TEST_F(SnakeFixture, canAddSegments) {
-  EXPECT_EQ(getSnake().getSnakeSegments().size(), 0);
+  EXPECT_EQ(getSnake().getSnakeSegments().size(), k0);
   getSnake().addSegment(Game::Segment{Game::BoardPosition{}});
-  EXPECT_EQ(getSnake().getSnakeSegments().size(), 1);
+  EXPECT_EQ(getSnake().getSnakeSegments().size(), k1);
   getSnake().addSegment(Game::Segment{Game::BoardPosition{}});
   getSnake().addSegment(Game::Segment{Game::BoardPosition{}});
-  EXPECT_EQ(getSnake().getSnakeSegments().size(), 3);
+  EXPECT_EQ(getSnake().getSnakeSegments().size(), k3);
 }
 
 } // namespace
